ff0c1d76a07dbf67ecdbc7bc7682f1cc98099dc1
Ayman <aymanm979@gmail.com>
2021-12-10 13:19:20 -0800

    added console log to submit button

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+function toggledark(){
+  togglebtn = document.getElementsByClassName('toggle');
+  togglebtn.addEventListener('change',()=>{
+    const dark = document.body;
+    dark.classList.toggle('dark')
+  }
+  )
+}
+submitbtn = document.getElementById('frm-btn')
+submitbtn.addEventListener('click', function(i){
+    i.preventdefault()
+    console.log(getElementById('name').value,getElementById('email').value,getElementById('msg').value)
+})
